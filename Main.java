af3d77912d5884573c81104bec927078febb0238
Patrick-Punch <137859615+Patrick-Punch@users.noreply.github.com>
2023-12-06 16:59:51 -0800

    corrected date

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@
  * The IntList project uses a int[] as a data set, and the myIntList class implements the intList interface, allowing it to perform data management operations run by the testIntList class.
  * @author Patrick Punch
  * @version 1.0
- * @since 12/3/2023
+ * @since 12/6/2023
  */
 
 public class Main extends TestIntList {
